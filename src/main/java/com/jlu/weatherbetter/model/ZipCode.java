d952d036f09ee671408dc6592a6f25625f02dd2f
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-04 14:43:04 -0600

    Add zip code model and repo

---
+++ src/main/java/com/jlu/weatherbetter/model/ZipCode.java
@@ -0,0 +1,32 @@
+package com.jlu.weatherbetter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ZipCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    private String zipCode;
+
+    public ZipCode(String zipCode) {
+        this.zipCode=zipCode;
+    }
+}
---
+++ src/main/java/com/jlu/weatherbetter/repository/ZipCodeRepository.java
@@ -0,0 +1,10 @@
+package com.jlu.weatherbetter.repository;
+
+import com.jlu.weatherbetter.model.ZipCode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ZipCodeRepository extends PagingAndSortingRepository<ZipCode, Long> {
+    Page<ZipCode> findAll(Pageable page);
+}
