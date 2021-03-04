8d37cee4d9f8e4b0c17394f346fefcd5e7438ff3
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-04 08:44:04 -0600

    Create initial project files

---
+++ src/main/java/com/jlu/weatherbetter/WeatherBetterApplication.java
@@ -0,0 +1,13 @@
+package com.jlu.weatherbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlu/weatherbetter/WeatherBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlu.weatherbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
