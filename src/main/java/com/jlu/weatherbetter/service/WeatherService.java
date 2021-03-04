fd401819c49f0aedec40c578ae46918482b09ed9
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-04 09:41:48 -0600

    Add WeatherService file

---
+++ src/main/java/com/jlu/weatherbetter/service/WeatherService.java
@@ -0,0 +1,19 @@
+package com.jlu.weatherbetter.service;
+
+import com.jlu.weatherbetter.model.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+    @Value("${api_key")
+    private String apiKey;
+
+    public Response getForecast(String zipCode){
+        String url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode +
+                "&units=imperial&appid=" + apiKey;
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, Response.class);
+    }
+}
