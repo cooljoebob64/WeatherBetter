2e8b05e7107da166ac6991b1ab2a62b903d1f22d
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-04 14:50:17 -0600

    Save searches to database with each search

--- src/main/java/com/jlu/weatherbetter/service/WeatherService.java
+++ src/main/java/com/jlu/weatherbetter/service/WeatherService.java
@@ -1,6 +1,9 @@
 package com.jlu.weatherbetter.service;
 
 import com.jlu.weatherbetter.model.Response;
+import com.jlu.weatherbetter.model.ZipCode;
+import com.jlu.weatherbetter.repository.ZipCodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -12,12 +15,17 @@ public class WeatherService {
     @Value("${api_key}")
     private String apiKey;
 
+    @Autowired
+    ZipCodeRepository zipCodeRepository;
+
     public Response getForecast(String zipCode) {
         String url = "http://api.openweathermap.org/data/2.5/weather?zip=" +
                 zipCode + "&units=imperial&appid=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
 
+
         try {
+            zipCodeRepository.save(new ZipCode(zipCode));
             return restTemplate.getForObject(url, Response.class);
         }
         catch (HttpClientErrorException ex) {
